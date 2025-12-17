01e41f7fc6d51a24a3e70409c72ee074dde827ab
johnandelin <john.e.andelin@gmail.com>
2025-12-17 15:27:03 -0700

    Update whats_cooking_ML.R

--- whats_cooking_ML.R
+++ whats_cooking_ML.R
@@ -3,8 +3,6 @@ library(tidymodels)
 library(textrecipes)
 library(jsonlite)
 
-
-# Load Data
 train <- fromJSON("train.json", flatten = TRUE) |>
   as_tibble() |>
   mutate(ingredients = map_chr(ingredients, paste, collapse = " "))
@@ -13,99 +11,47 @@ test <- fromJSON("test.json", flatten = TRUE) |>
   as_tibble() |>
   mutate(ingredients = map_chr(ingredients, paste, collapse = " "))
 
-
-# Train / Validation Split
-split <- initial_split(train, prop = 0.8, strata = cuisine)
-
-train_data <- training(split)
-valid_data <- testing(split)
-
-
-# Recipe (TF-IDF)
-recipe_spec <- recipe(cuisine ~ ingredients, data = train_data) |>
+rec <- recipe(cuisine ~ ingredients, data = train) |>
   step_tokenize(ingredients) |>
   step_tokenfilter(ingredients, max_tokens = 6000) |>
   step_tfidf(ingredients)
 
-
-# Tunable Model (glmnet)
-model_spec <- multinom_reg(
+mod <- multinom_reg(
   penalty = tune(),
   mixture = tune()
 ) |>
   set_engine("glmnet") |>
   set_mode("classification")
 
-
-# Workflow
 wf <- workflow() |>
-  add_recipe(recipe_spec) |>
-  add_model(model_spec)
+  add_recipe(rec) |>
+  add_model(mod)
 
+folds <- vfold_cv(train, v = 5, strata = cuisine)
 
-# Cross-Validation
-folds <- vfold_cv(train_data, v = 5, strata = cuisine)
-
-
-# Tuning Grid
 grid <- grid_regular(
-  penalty(range = c(-4, -1)),   # 1e-4 to 1e-1
+  penalty(range = c(-4, -1)),
   mixture(range = c(0.5, 1)),
-  levels = c(6, 4)
+  levels = 5
 )
 
-
-# Tune Model
-tuned_results <- tune_grid(
+tuned <- tune_grid(
   wf,
   resamples = folds,
   grid = grid,
-  metrics = metric_set(accuracy),
-  control = control_grid(save_pred = TRUE)
+  metrics = metric_set(accuracy)
 )
 
-
-# Best Parameters
-best_params <- tuned_results |>
+best_params <- tuned |>
   select_best("accuracy")
 
-
-# Validate Performance
 final_wf <- wf |>
   finalize_workflow(best_params)
 
 final_fit <- final_wf |>
-  fit(train_data)
-
-validation_metrics <- final_fit |>
-  predict(valid_data) |>
-  bind_cols(valid_data) |>
-  metrics(truth = cuisine, estimate = .pred_class)
-
-
-# Train Final Model on Full Data
-final_recipe <- recipe(cuisine ~ ingredients, data = train) |>
-  step_tokenize(ingredients) |>
-  step_tokenfilter(ingredients, max_tokens = 6000) |>
-  step_tfidf(ingredients)
-
-final_model <- multinom_reg(
-  penalty = best_params$penalty,
-  mixture = best_params$mixture
-) |>
-  set_engine("glmnet") |>
-  set_mode("classification")
-
-final_wf_full <- workflow() |>
-  add_recipe(final_recipe) |>
-  add_model(final_model)
-
-final_fit_full <- final_wf_full |>
   fit(train)
 
-
-# Predict Test + Submission
-submission <- final_fit_full |>
+submission <- final_fit |>
   predict(test) |>
   bind_cols(test |> select(id)) |>
   rename(cuisine = .pred_class)
